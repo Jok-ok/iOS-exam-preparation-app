869c97f4b10b7816618ce53db89c93590abe785e
Jok-ok <nujniy.vorobey@gmail.com>
2023-04-01 09:50:18 +0200

    Initial Commit

---
+++ ExamPreparationSchedule/ExamPreparationScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamPreparationScheduleApp.swift
+//  ExamPreparationSchedule
+//
+//  Created by Александр Воробей on 01.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExamPreparationScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
